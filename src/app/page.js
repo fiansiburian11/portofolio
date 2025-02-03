1d2c1681f6d75cb12dbbcc4f59477b7c6a94735c
FirmanRepo <bukanfirman11@gmail.com>
2025-02-03 14:35:20 +0700

    Update page.js

--- src/app/page.js
+++ src/app/page.js
@@ -13,7 +13,7 @@ export const metadata = {
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col bg-[#121212]">
+    <main className="flex min-h-screen flex-col bg-[#121212] overflow-x-hidden">
       <Navbar />
       <div className="container mt-24 mx-auto px-12 py-4">
         <HeroSection />
